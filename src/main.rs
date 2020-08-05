4011f64232553739f469aa5ca5958620bb3b9cab
Uğurcan Akkök <ucak_gestalt@protonmail.com>
2020-08-05 11:28:56 +0300

    Added clap crate to handle cli args

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,51 @@
-use std::{env, io::stdout};
+use clap::{crate_authors, crate_version, App, Arg};
 #[allow(unused_imports)]
-use log::{warn, info, error, debug, trace};
-use simplelog::{WriteLogger, LevelFilter};
+use log::{debug, error, info, trace, warn};
+use simplelog::{LevelFilter, WriteLogger};
+use std::{env, io::stdout};
+
 mod cpu;
-mod mem;
-mod utils;
-use utils::{usage};
-use mem::Memory;
 use cpu::Cpu;
+mod mem;
+use mem::Memory;
 
 fn main() {
-    let mut args = env::args();
-    if args.len() < 2 {
-        usage();
-        return;
-    }
-    args.next();
-    let mut romname = String::default();
-    let mut dump = false;
-    loop {
-        match args.next() {
-            Some(arg) => {
-                if arg.starts_with("-h") {
-                    usage();
-                }
-                if arg.starts_with("-l") {
-                    //TODO set log level
-                }
-                if arg.starts_with("-x") {
-                    dump = true;
-                } else {
-                    romname = arg;
-                }
-            }
-            None => break,
-        }
-    }
-    WriteLogger::init(LevelFilter::Off, simplelog::Config::default(), stdout()).unwrap();
+    let matches = App::new("ruBoy")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("A Gameboy emulator written in Rust")
+        .arg(
+            Arg::with_name("log")
+                .short("l")
+                .long("log-level")
+                .value_name("LEVEL")
+                .possible_values(&["off", "error", "warn", "debug", "info", "trace", "all"])
+                .default_value("all")
+                .number_of_values(1)
+                .help("The verbosity level of logs"),
+        )
+        .arg(
+            Arg::with_name("rom")
+                .help("Set the rom file to use")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+    let log_level = matches.value_of("log").unwrap_or("warn");
+    let log_level = match log_level {
+        "off" => LevelFilter::Off,
+        "error" => LevelFilter::Error,
+        "warn" => LevelFilter::Warn,
+        "debug" => LevelFilter::Debug,
+        "info" => LevelFilter::Info,
+        "trace" => LevelFilter::Trace,
+        _ => LevelFilter::Trace,
+    };
+    WriteLogger::init(log_level, simplelog::Config::default(), stdout()).unwrap();
 
-    if dump {
-        // disassemble(&romname);
-    } else {
-        let mut memory = Memory::default();
-        let cartridge = memory.load_rom(&romname);
-        let mut cpu = Cpu::new(memory);
-        cpu.run(cartridge);
-    }
+    let romname = matches.value_of("rom").expect("Rom file need to be specified");
+    let mut memory = Memory::default();
+    let cartridge = memory.load_rom(&romname);
+    let mut cpu = Cpu::new(memory);
+    cpu.run(cartridge);
 }
